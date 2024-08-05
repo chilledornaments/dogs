deff318c93955d77fd97d300602091be3e325f8d
chilledornaments <mitch@chilledornaments.com>
2024-08-05 14:11:16 -0600

    add web assets file

---
+++ terraform/web.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.images.bucket
+  key    = "index.html"
+  source = "${path.module}/../web/index.html"
+  etag   = filemd5("${path.module}/../web/index.html")
+}
+
+resource "aws_s3_object" "favicon" {
+  bucket = aws_s3_bucket.images.bucket
+  key    = "favicon.ico"
+  source = "${path.module}/../web/favicon.ico"
+  etag   = filemd5("${path.module}/../web/favicon.ico")
+}
