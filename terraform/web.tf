7eb0efeab143283eda67bc065d7fac619e72f004
chilledornaments <mitch@chilledornaments.com>
2024-08-05 14:20:48 -0600

    add content_type

--- terraform/web.tf
+++ terraform/web.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.images.bucket
-  key    = "index.html"
-  source = "${path.module}/../web/index.html"
-  etag   = filemd5("${path.module}/../web/index.html")
+  bucket       = aws_s3_bucket.images.bucket
+  key          = "index.html"
+  source       = "${path.module}/../web/index.html"
+  etag         = filemd5("${path.module}/../web/index.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "favicon" {
