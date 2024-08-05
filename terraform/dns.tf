7317e49eadf36c75e5742c09e6e0549f7ac6e222
chilledornaments <mitch@chilledornaments.com>
2024-08-04 22:32:10 -0600

    add v6 record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "images" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "images_v6" {
+  zone_id = data.aws_route53_zone.dogs.id
+  name    = "img.${data.aws_route53_zone.dogs.name}"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_cloudfront_distribution.images.domain_name
+    zone_id                = aws_cloudfront_distribution.images.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
