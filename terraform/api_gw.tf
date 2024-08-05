3a03d1b90bfd2d85c9143dc0b1cd5286a86ff856
chilledornaments <mitch@chilledornaments.com>
2024-08-05 09:43:59 -0600

    add stage

--- terraform/api_gw.tf
+++ terraform/api_gw.tf
@@ -60,3 +60,9 @@ resource "aws_api_gateway_deployment" "link_retriever" {
     create_before_destroy = true
   }
 }
+
+resource "aws_api_gateway_stage" "v1" {
+  deployment_id = aws_api_gateway_deployment.link_retriever.id
+  rest_api_id   = aws_api_gateway_rest_api.link_retriever.id
+  stage_name    = "v1"
+}
