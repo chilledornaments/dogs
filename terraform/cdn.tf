6c4024dd1f7954e779693b1e8b15a6c2c5e37c4a
chilledornaments <mitch@chilledornaments.com>
2024-08-05 13:44:05 -0600

    add resource

--- terraform/cdn.tf
+++ terraform/cdn.tf
@@ -2,6 +2,14 @@ locals {
   s3_origin_id = "images-bucket"
 }
 
+resource "aws_cloudfront_origin_access_control" "images" {
+  name                              = "dog-images"
+  description                       = "Allow dog images CloudFront to read images and HTML"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 resource "aws_cloudfront_distribution" "images" {
   enabled             = true
   is_ipv6_enabled     = true
@@ -13,8 +21,9 @@ resource "aws_cloudfront_distribution" "images" {
   aliases = concat([aws_acm_certificate.images.domain_name], tolist(aws_acm_certificate.images.subject_alternative_names))
 
   origin {
-    domain_name = aws_s3_bucket.images.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
+    domain_name              = aws_s3_bucket.images.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+    origin_access_control_id = aws_cloudfront_origin_access_control.images.id
   }
 
   restrictions {
